65224fc5bde0cba0404c86c59330399af29d165f
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2020-08-22 15:26:31 +0700

    add dotenv and more
    - fix how app start

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
 import "apollo-cache-control";
-import server from "./server";
+import "dotenv/config";
+import { httpServer, server } from "./server";
 
+const PORT = process.env.PORT || 4000;
 async function main() {
-  const { url } = await server.listen();
-  console.log(`🚀 HN GraphQL server ready at ${url}`);
+  httpServer.listen(PORT, () => {
+    console.log(
+      `🚀 HN GraphQL at http://localhost:${PORT}${server.graphqlPath}`
+    );
+    console.log(
+      `🚀 HN GraphQL updates ready at ws://localhost:${PORT}${server.subscriptionsPath}`
+    );
+  });
 }
 
 main();
