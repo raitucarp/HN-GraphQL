b7293b0f84992d90349dbc4b52f679b645b036c4
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2020-08-23 08:58:08 +0700

    fix userinfo resolver and add urlmeta resolver

--- src/resolvers/item.ts
+++ src/resolvers/item.ts
@@ -1,15 +1,5 @@
 import { Item } from "../data-sources/hackernews";
 
-export const userInfoResolver = async (
-  item: Item,
-  _args: any,
-  { dataSources }: any
-) => {
-  const username = item.by;
-  const user = dataSources.hackerNewsAPI.getUser(username);
-  return user;
-};
-
 export const commentsResolver = async (
   item: Item,
   { offset, limit }: any,
