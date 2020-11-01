6577f0078e839eb05c447f5c3e33c27796ee9bf0
Ribhararnus Pracutiar <ribhararnus.pracutiar@gmail.com>
2020-11-01 14:10:33 +0700

    style: remove unnecessary lines

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,5 @@
 import { ApolloServer } from "apollo-server-express";
-import responseCachePlugin from "apollo-server-plugin-response-cache";
-import { HackerNewsAPI } from "./data-sources/hackernews";
 import { RedisCache } from "apollo-server-cache-redis";
-import resolvers from "./resolvers";
-import typeDefs from "./typedefs";
-import http from "http";
-import express from "express";
-import { URLMetaAPI } from "./data-sources/urlmeta";
 import { REQUEST_CACHE_TTL, REDIS_CONNECTION_STRING } from "./config/from-env";
 import express from "express";
 import http from "http";
@@ -17,16 +10,10 @@ import { HackerNewsAPI } from "./data-sources/hackernews";
 import { ITEMS_UPDATED, PROFILES_UPDATED, pubsub } from "./resolvers";
 
 const app = express();
-const dataSources = () => ({
-  hackerNewsAPI: new HackerNewsAPI(),
-  urlMetaAPI: new URLMetaAPI(),
-});
-
 const redisCache = new RedisCache(REDIS_CONNECTION_STRING);
 
 export const server = new ApolloServer({
   schema,
-  resolvers,
   tracing: true,
   cache: redisCache,
   cacheControl: {
